0858c6dde21c2a145a6621ea94a7de2ee5d30dc4
Cat-Denton <willwdenton@gmail.com>
2021-05-21 12:41:36 -0700

    add Program.cs Startup.cs HairSalon.csproj. Add Client.cs HairSalonContext.cs and Stylist.cs models

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string ContactInfo { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
